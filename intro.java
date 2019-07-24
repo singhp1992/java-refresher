9d70460a623ed90cbebaf8143ef682e614c727dd
Preeti <singhp1992@gmail.com>
2019-07-24 10:52:08 +0200

    able to print name in console

--- intro.java
+++ intro.java
@@ -1,8 +1,10 @@
 import java.io.Console;
 
-public class Introductions {
+public class intro {
 
     public static void main(String[] args) {
         Console console = System.console();
+
+        console.printf("Hello, my name is Preeti");
     }
 }
