35131c3b199a462cb72778753969472660a83fa7
Preeti <singhp1992@gmail.com>
2019-07-24 12:02:24 +0200

    added condiitonal for bad language

--- TreeStory.java
+++ TreeStory.java
@@ -5,15 +5,24 @@ public class TreeStory {
     public static void main(String[] args) {
         Console console = System.console();
 
-        int age = 12;
+        String ageAsString = console.readLine("How old are you? ");
+
+        int age = Integer.parseInt(ageAsString);
 
         if (age < 13) {
-            
+            console.printf("Sorry! You must be at least 13 to use this program .\n");
+            System.exit(0);
         }
 
         String name = console.readLine("Enter a name:  ");
         String adjective = console.readLine("Enter an adjective:  ");
         String noun = console.readLine("Enter a noun:  ");
+
+        if(noun.equals("dork")){
+            console.printf("Please be nice");
+            System.exit(0);
+        }
+
         String adverb = console.readLine("Enter an adverb:  ");
         String verb = console.readLine("Enter a verb ending with -ing:  ");
 
