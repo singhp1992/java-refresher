23e98030a7f83c93666e35c3fedae7c47ce9d46d
Preeti <singhp1992@gmail.com>
2019-07-24 11:08:54 +0200

    added readline

--- intro.java
+++ intro.java
@@ -5,9 +5,7 @@ public class intro {
     public static void main(String[] args) {
         Console console = System.console();
 
-        String firstName = "Simon";
-
-
+        String firstName = console.readLine("What is your name? ");
 
         console.printf("Hello, my name is %s\n", firstName);
         console.printf("%s is learning Java\n", firstName);
