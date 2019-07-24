5c1dd163bdf740afef0d7ace819733405894ca72
Preeti <singhp1992@gmail.com>
2019-07-24 11:21:53 +0200

    declared name and adjective variable

--- TreeStory.java
+++ TreeStory.java
@@ -5,6 +5,7 @@ public class TreeStory {
     public static void main(String[] args) {
         Console console = System.console();
 
-    
+        String name = console.readLine("Enter your name: ");
+        String adjective = console.readLine("Give me and adjective: ");
     }
 }
