80d53e8c9a9509741029a901790d7c810d9ddd45
Preeti <singhp1992@gmail.com>
2019-07-24 11:31:20 +0200

    removed string variables

--- TreeStory.java
+++ TreeStory.java
@@ -5,9 +5,6 @@ public class TreeStory {
     public static void main(String[] args) {
         Console console = System.console();
 
-        String name = console.readLine("Enter your name: ");
-        String adjective = console.readLine("Give me and adjective: ");
-
-        console.printf("%s is a very %s", name, adjective);
+        
     }
 }
