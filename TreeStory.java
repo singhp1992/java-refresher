aa4cf9474a018496f28df17fbcf150ae4f0635b7
Preeti <singhp1992@gmail.com>
2019-07-24 11:31:47 +0200

    added variables

--- TreeStory.java
+++ TreeStory.java
@@ -5,6 +5,11 @@ public class TreeStory {
     public static void main(String[] args) {
         Console console = System.console();
 
-        
+        String name = console.readLine("Enter a name:  ");
+        String adjective = console.readLine("Enter an adjective:  ");
+        String noun = console.readLine("Enter a noun:  ");
+        String adverb = console.readLine("Enter an adverb:  ");
+        String verb = console.readLine("Enter a verb ending with -ing:  ");
+
     }
 }
